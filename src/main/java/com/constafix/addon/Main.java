489835671cbd8966d3a24fcfa701894aa6523a83
constafix <dk2513086@yandex.ru>
2025-05-11 18:50:02 +0300

    v.0.0.1test

--- src/main/java/com/constafix/addon/Main.java
+++ src/main/java/com/constafix/addon/Main.java
@@ -2,7 +2,7 @@ package com.constafix.addon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java-сервер для Home Assistant запущен!");
+        System.out.println("Java-server for Home Assistant has been running!");
         // Здесь твоя логика
     }
 }
